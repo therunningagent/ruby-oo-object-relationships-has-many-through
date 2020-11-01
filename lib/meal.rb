23284875be763d71945fe631c36a5fb8c49edd20
therunningagent <luistjr@gmail.com>
2020-11-01 17:34:27 -0500

    finished lab

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,24 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
 
+    @@all = [] 
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def meals
+        Meals.all.select do |meal| meal.customer == self 
+        end 
+    end 
+
+    
 end
